d2938244ec5099ee9057ad4ce2dadd0be5add7fe
bassamnegm <bassamnegm@gmail.com>
2026-02-22 11:39:31 +0200

    feat: initialize web application with React and Vite
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in the public directory.
    - Created About component for the application.
    - Added App.css for styling the application.
    - Implemented App component with placeholders for future components.
    - Developed Home component with styled heading.
    - Added React logo SVG in the assets directory.
    - Created home.css for Home component styling.
    - Initialized index.css for global styles.
    - Set up main entry point for the application with ReactDOM.
    - Configured Vite with React plugin in vite.config.js.

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Home from './Home'
+import About from './About'
+function App() {
+  // put your logic
+  let p=<p>iti aswan</p>;
+  console.log(p);
+  return (
+    <div></div>
+    //  <Header></Header>
+    //  <main className='col'>
+    //   <Left/>
+    //   <Right/>
+    //  </main>
+  
+    //   <Footer></Footer>
+  // <div id='app'>
+  //   <Home/>
+  //   <About/>
+  // </div>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import './home.css';
+function Home()
+{
+    return(
+        <div className="cls">
+            <h1 className='home' style={{color:"red",textDecoration:"underline"}}>Home</h1>
+        </div>
+    )
+}
+
+export default Home;
